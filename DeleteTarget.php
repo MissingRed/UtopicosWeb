2272b5002d7231aa207bc926960455cd85b08f55
sirzes02 <sirzes02@gmail.com>
2020-05-03 18:57:53 -0500

    final - cambio de db - header allow

--- DeleteTarget.php
+++ DeleteTarget.php
@@ -1,4 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header(
+  "Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
+);
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
 require_once 'HTTP/Request2.php';
 require_once 'SignatureBuilder.php';
 
@@ -6,8 +13,8 @@ new DeleteTarget($_GET["targetId"], $_GET["targetName"]);
 
 class DeleteTarget
 {
-  private $access_key = "79555486c9a2a4a421f47fe4dcada9ede670c9bc";
-  private $secret_key = "b84d1921cbf5c3de85434ad6a1c13531ee107b9b";
+  private $access_key = "872193617f52547044d05e2ccf372c29cc65ed1b";
+  private $secret_key = "4d5f0c38a2322404f163f9ab6dc9b1b82525986d";
 
   private $url = "https://vws.vuforia.com";
   private $requestPath = "/targets/";
@@ -37,6 +44,7 @@ class DeleteTarget
 
       if (200 == $response->getStatus() && file_exists($targetName . ".jpg")) {
         unlink($targetName . ".jpg");
+        unlink($targetName . ".mp4");
       }
 
       echo $response->getBody();
